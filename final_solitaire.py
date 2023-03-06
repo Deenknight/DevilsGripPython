43145da4fb6842c629ee5138a6694d01e2aeb6c0
Deenknight <Deenknight21@hotmail.com>
2023-03-06 16:33:07 -0700

    commit for the sake of committing

--- final_solitaire.py
+++ final_solitaire.py
@@ -1,18 +1,48 @@
-from PyQt5.QtGui import QImage, QPixmap, QColor, QBrush, QPen
-from PyQt5.QtWidgets import QGraphicsPixmapItem, QGraphicsItem, QGraphicsRectItem, QMainWindow, QGraphicsView, QGraphicsScene, QAction, QMessageBox, QApplication
+from PyQt5.QtGui import QImage, QPixmap, QColor, QBrush, QPen   
+from PyQt5.QtWidgets import QGraphicsPixmapItem, QGraphicsItem, QGraphicsRectItem, QMainWindow, QGraphicsView, QGraphicsScene, QAction, QMessageBox, QApplication, QGraphicsTextItem
 from PyQt5.QtCore import QSize, QRect, pyqtSignal, QObject, QRectF, QPointF, Qt
-import os
+import os, sys
 import random
+import copy
+
+"""
+#TODO:
+    - Win condition check
+    - Win messages
+    - Card Audio
+    - Win Audio
+
+"""
 
 # Setting global variables 
-win_w_h = 1200, 800  # window's width and height. 
-card_spacer_x = 130  # distance between cards (x).
-g_offset_x = 50  # global offset of x. 
-g_offset_y = 50  # global offset of y. 
+win_w_h = 1600, 1000  # window's width and height. 
+card_spacer_x = 180  # distance between cards (x).
+card_spacer_y = 50
+g_offset_x = 100  # global offset of x. 
+g_offset_y = 100  # global offset of y. 
 face_side = 0  # face side of a card.
 back_side = 1  # back side of a card.
-suits = ["C", "S", "H", "D"]  # suits of a cards.
+suits = ["_of_clubs", "_of_spades", "_of_hearts", "_of_diamonds"]  # suits of a cards.
+COLUMNS = 8
+ROWS = 3
+CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+DECKS = 2
 
+CARD_SCALE = 1.25
+
+CARD_WIDTH = int(80*CARD_SCALE)
+CARD_HEIGHT = int(116*CARD_SCALE)
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class Signals(QObject):
     '''
@@ -20,6 +50,8 @@ class Signals(QObject):
     '''
     complete = pyqtSignal()
     clicked = pyqtSignal()
+    decrement_counter = pyqtSignal()
+    get_from_deal = pyqtSignal()
 
 
 class Card(QGraphicsPixmapItem):
@@ -36,6 +68,7 @@ class Card(QGraphicsPixmapItem):
         self.value = value
         self.suit = suit
         self.side = None
+
         # Cards have no internal transparent areas, so we can use this faster method.
         self.setShapeMode(QGraphicsPixmapItem.BoundingRectShape)
         self.setFlag(QGraphicsItem.ItemIsMovable)
@@ -44,8 +77,8 @@ class Card(QGraphicsPixmapItem):
 
     # Loading images of cards (face and back)
     def load_images(self):
-        self.face = QPixmap(os.path.join('cards', '%s%s.png' % (self.value, self.suit)))
-        self.back = QPixmap(os.path.join('images', 'card_back.png'))
+        self.face = QPixmap(resource_path(os.path.join('cards', '%s%s.png' % (self.value, self.suit)))).scaled(CARD_WIDTH, CARD_HEIGHT, Qt.KeepAspectRatio, Qt.FastTransformation)
+        self.back = QPixmap(resource_path(os.path.join('images', 'card_back.png'))).scaled(CARD_WIDTH, CARD_HEIGHT, Qt.KeepAspectRatio, Qt.FastTransformation)
 
     def turn_face_up(self):
         self.side = face_side
@@ -64,10 +97,10 @@ class Card(QGraphicsPixmapItem):
         return 'r' if self.suit in ('H', 'D') else 'b'
 
     def mousePressEvent(self, e):
-        if not self.is_face_up and self.stack.cards[-1] == self:
-            self.turn_face_up()  # We can do this without checking.
-            e.accept()
-            return
+        # if not self.is_face_up and self.stack.cards[-1] == self:
+        #     self.turn_face_up()  # We can do this without checking.
+        #     e.accept()
+        #     return
 
         if self.stack and not self.stack.is_free_card(self):
             e.ignore()
@@ -90,13 +123,31 @@ class Card(QGraphicsPixmapItem):
                     if item.stack.is_valid_drop(self):
                         # Remove card + all children from previous stack, add to the new.
                         # Note: the only place there will be children is on a workstack.
+                        
+                        prev_stack_type = type(self.stack)
+
                         cards = self.stack.remove_card(self)
                         item.stack.add_cards(cards)
-                        break
 
-        # Refresh this card's stack, pulling it back if it was dropped.
-        self.stack.update()
+                        if prev_stack_type == Deal:
+                            self.signals.decrement_counter.emit()
+
+                         # Refresh this card's stack, pulling it back if it was dropped.
+                        self.stack.update()
+                        
+                        break
+                    elif type(self.stack) == Work and type(item.stack) == Work and not item.stack.is_complete:
+                        self.stack.swapCards(item.stack)
+                        break
+            else:
+                self.stack.update()
+        else:
+            self.stack.update()
+       
+        
         super(Card, self).mouseReleaseEvent(e)
+        
+
 
 class Stack(QGraphicsRectItem):
     '''
@@ -106,7 +157,7 @@ class Stack(QGraphicsRectItem):
     def __init__(self, *args, **kwargs):
         super(Stack, self).__init__(*args, **kwargs)
 
-        self.setRect(QRectF(QRect(0, 0, 80, 116)))
+        self.setRect(QRectF(QRect(0, 0, CARD_WIDTH, CARD_HEIGHT)))
         self.setZValue(-1)
         # Cards on this deck, in order.
         self.cards = []
@@ -161,8 +212,11 @@ class Deck(Stack):
     This class implements cards stacking into the deck, restaking and taking 
     a top card from the list of cards.
     '''
-    offset_x = -0.2
-    offset_y = -0.3
+
+
+    offset_x = -0.2/CARD_SCALE
+    offset_y = -0.3/CARD_SCALE
+
     restack_counter = 0
 
     def reset(self):
@@ -212,14 +266,22 @@ class Deck(Stack):
     def is_valid_drop(self, card):
         return False
 
+    def get_cards_left(self):
+        return len(self.cards)
+
 
 class Deal(Stack):
     '''
     This class implements a start of a new game. 
     '''
-    offset_x = 20
+    offset_x = 15*CARD_SCALE
     offset_y = 0
     spread_from = 0
+
+    def __init__(self, deckstack, ):
+        super().__init__()
+
+        self.deckStack = deckstack
 
     def setup(self):
         self.setPen(QPen(Qt.NoPen))
@@ -255,10 +317,11 @@ class Work(Stack):
     removing card. 
     '''
     offset_x = 0
-    offset_y = 15
+    offset_y = 15*CARD_SCALE
     offset_y_back = 5
 
     def setup(self):
+        self.signals = Signals()
         self.setPen(QPen(Qt.NoPen))
         color = QColor(Qt.black)
         color.setAlpha(50)
@@ -269,8 +332,8 @@ class Work(Stack):
         if not self.cards:
             return True
 
-        if (card.color != self.cards[-1].color and
-            card.value == self.cards[-1].value -1):
+        if (card.suit == self.cards[-1].suit and
+            card.value == self.cards[-1].value + 3):
             return True
 
         return False
@@ -281,10 +344,17 @@ class Work(Stack):
     def add_card(self, card, update=True):
         if self.cards:
             card.setParentItem(self.cards[-1])
+            
         else:
             card.setParentItem(self)
 
         super(Work, self).add_card(card, update=update)
+
+        if self.is_complete:
+            for card in self.cards:
+                card.turn_back_up()
+
+            self.signals.complete.emit()
 
     def remove_card(self, card):
         index = self.cards.index(card)
@@ -294,14 +364,29 @@ class Work(Stack):
             # Remove card and all children, returning a list of cards removed in order.
             card.setParentItem(None)
             card.stack = None
+        
+        if not self.cards:
+            self.signals.get_from_deal.emit(self)
+            if self.deckStack.cards:
+                card = self.deckStack.take_top_card()
+                if card:
+                    self.add_card(card)
+                    card.turn_face_up()
+                    self.signals.decrement_counter.emit()
+
+            elif self.deckStack.can_restack(self.rounds_n):
+                self.deckStack.restack(self.dealstack)
+                self.deckStack.update_stack_status(self.rounds_n)
 
         self.update()
+
         return cards
 
     def remove_all_cards(self):
         for card in self.cards[:]:
             card.setParentItem(None)
             card.stack = None
+            card.setPos(QPointF(-1000, -1000))
         self.cards = []
 
     def update(self):
@@ -316,14 +401,41 @@ class Work(Stack):
             else:
                 offset_y = self.offset_y_back
 
+    def getCards(self):
+        deepcopy = []
+        for card in self.cards:
+            deepcopy.append(Card(card.value, card.suit))
+        return deepcopy
+    
+    def swapCards(self, otherStack):
+        otherStackCards = otherStack.getCards()
+        currentStackCards = self.getCards()
+        
+        self.remove_all_cards()
+        for card in otherStackCards:
+            self.add_card(card, False)
+            card.turn_face_up()
+
+        otherStack.remove_all_cards()
+        for card in currentStackCards:
+            otherStack.add_card(card, False)
+            card.turn_face_up()
+
+        otherStack.update()
+        self.update()
+    
+    @property
+    def is_complete(self):
+        return len(self.cards) == 4
+    
 
 class Drop(Stack):
     '''
     This class implements drop stack setup process including adding and 
     removing card. 
     '''
-    offset_x = -0.2
-    offset_y = -0.3
+    offset_x = -0.2*CARD_SCALE
+    offset_y = -0.3*CARD_SCALE
 
     suit = None
     value = 0
@@ -372,7 +484,7 @@ class Trigger(QGraphicsRectItem):
     '''
     def __init__(self, *args, **kwargs):
         super(Trigger, self).__init__(*args, **kwargs)
-        self.setRect(QRectF(QRect(30, 30, 110, 140)))
+        self.setRect(QRectF(QRect(g_offset_x, g_offset_y, CARD_WIDTH, CARD_HEIGHT)))
         self.setZValue(1000)
         pen = QPen(Qt.NoPen)
         self.setPen(pen)
@@ -394,7 +506,7 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.scene.setSceneRect(QRectF(0, 0, *win_w_h))
 
-        felt = QBrush(QPixmap(os.path.join('images','background.jpg')))
+        felt = QBrush(QPixmap(resource_path(os.path.join('images','background.jpg'))))
         self.scene.setBackgroundBrush(felt)
 
         view.setScene(self.scene)
@@ -411,46 +523,54 @@ class MainWindow(QMainWindow):
         quit_action.triggered.connect(self.quit)
         menu.addAction(quit_action)
 
-        self.deck = []
-        self.deal_n = 1  # Number of cards to deal each time
-        self.rounds_n = None  # Number of rounds (restacks) before end.
 
-        for suit in suits:
-            for value in range(1, 14):
-                card = Card(value, suit)
-                self.deck.append(card)
-                self.scene.addItem(card)
+        
+
+        self.deck = []
+        self.deal_n = 3  # Number of cards to deal each time
+        self.rounds_n = None  # Number of rounds (restacks) before end.
+        self.cardStackSpace = 10
+
+        for i in range(DECKS):
+            for suit in suits:
+                for value in CARDS:
+                    card = Card(value, suit)
+                    card.signals.decrement_counter.connect(self.decrement_deck_counter)
+                    self.deck.append(card)
+                    self.scene.addItem(card)
+
 
         self.setCentralWidget(view)
         self.setFixedSize(*win_w_h)
+
+        self.cardCountText = QGraphicsTextItem("Cards left: " + str(72))
+        self.cardCountText.setScale(3)
+        self.cardCountText.setDefaultTextColor(QColor(255, 255, 255))
+        self.cardCountText.setPos(win_w_h[0] - 500, g_offset_y)
+        
+        self.scene.addItem(self.cardCountText)
 
         self.deckstack = Deck()
         self.deckstack.setPos(g_offset_x , g_offset_y)
         self.scene.addItem(self.deckstack)
 
-        # Set up the working locations.
-        self.works = []
-        for n in range(7):
-            stack = Work()
-            stack.setPos(g_offset_x + card_spacer_x*n, 200)
-            self.scene.addItem(stack)
-            self.works.append(stack)
-
         self.drops = []
 
-        # Set up the drop locations.
-        for n in range(4):
-            stack = Drop()
-            stack.setPos(g_offset_x + card_spacer_x * (3+n), g_offset_y)
-            stack.signals.complete.connect(self.check_win_condition)
-
-            self.scene.addItem(stack)
-            self.drops.append(stack)
-
         # Add the deal location.
-        self.dealstack = Deal()
+        self.dealstack = Deal(self.deckstack)
         self.dealstack.setPos(g_offset_x + card_spacer_x, g_offset_y)
         self.scene.addItem(self.dealstack)
+
+        # Set up the working locations.
+        self.works = []
+        for r in range(ROWS):
+            for n in range(COLUMNS):
+                stack = Work(self.deckstack, self.dealstack)
+                stack.setPos(g_offset_x + self.cardStackSpace*(n+1) + card_spacer_x*(n), g_offset_y + 200*(r+1))
+                stack.signals.complete.connect(self.check_win_condition)
+                stack.signals.decrement_counter.connect(self.decrement_deck_counter)
+                self.scene.addItem(stack)
+                self.works.append(stack)
 
         # Add the deal click-trigger.
         dealtrigger = Trigger()
@@ -468,12 +588,18 @@ class MainWindow(QMainWindow):
 
         if reply == QMessageBox.Yes:
             self.shuffle_and_stack()
+            self.cardCountText.setPlainText("Cards left: " + str(72))
+            self.cardCountText.update()
 
+    def decrement_deck_counter(self):
+        self.cardCountText.setPlainText("Cards left: " + str(len(self.deckstack.cards) + len(self.dealstack.cards)))
+        self.cardCountText.update()
+    
     def quit(self):
         self.close()
 
     def set_deal_n(self):
-        self.deal_n = 1
+        self.deal_n = 3
 
     def set_rounds_n(self):
         self.rounds_n = None  # setting unlimited rounds.
@@ -485,16 +611,16 @@ class MainWindow(QMainWindow):
             stack.reset()
 
         random.shuffle(self.deck)
-
+        
         # Deal out from the top of the deck, turning over the
         # final card on each line.
         cards = self.deck[:]
         for n, workstack in enumerate(self.works, 1):
-            for a in range(n):
+            for a in range(1):
                 card = cards.pop()
                 workstack.add_card(card)
                 card.turn_back_up()
-                if a == n-1:
+                if a == 0:
                     card.turn_face_up()
 
         # Ensure removed from all other stacks here.
@@ -521,12 +647,13 @@ class MainWindow(QMainWindow):
                 break
 
     def check_win_condition(self):
-        complete = all(s.is_complete for s in self.drops)
+        complete = all(s.is_complete for s in self.works)
+
         if complete:
             answer = QMessageBox.question(self, "Congratulations! You won!", 
             "Do you want to start a new game?", QMessageBox.Yes | QMessageBox.No)
-        if answer == QMessageBox.Yes:
-            self.shuffle_and_stack()
+            if answer == QMessageBox.Yes:
+                self.shuffle_and_stack()
 
 if __name__ == '__main__':
     app = QApplication([])
